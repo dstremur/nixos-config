3e8860258d7e0bb17aaaad1ac35e835f7d5eb0ab
Diego <diego@strebel.com>
2025-10-05 12:13:34 +0200

    flake update

--- nvidia.nix
+++ nvidia.nix
@@ -37,7 +37,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # package = config.boot.kernelPackages.nvidiaPackages.beta;
+    #	package = config.boot.kernelPackages.nvidiaPackages.latest;
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
       version = "580.82.09";
       sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
